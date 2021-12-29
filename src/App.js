de008bc708ce6e58ed4df1defe05be758b0fd919
Nithishvb <83941930+Nithishvb@users.noreply.github.com>
2021-12-29 03:11:29 -0800

    simple change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React from 'react'
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Hello CodeSandbox from react js</h1>
     </div>
   );
 }
